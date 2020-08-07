5dd6b3f5ea8f7b1120c31010cbf2cb075d1e5289
Kelvin Lima <kelvin_lima94@hotmail.com>
2020-08-07 01:43:59 -0300

    Reajuste Projeto Angular

--- ProAgil-App2/karma.conf.js
+++ ProAgil-App2/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/proag'),
+      dir: require('path').join(__dirname, './coverage/ProAgil-App'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
